d1e9965169ab36645ae26b796888259a0b7f9123
Sagar Vishwakarma <123996058+sagarv8090@users.noreply.github.com>
2024-02-03 16:43:08 +0530

    Add files via upload

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import BiryaniImg from "../../assets/biryani5.png";
+import Vector from "../../assets/vector3.png";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
+
+const Banner = () => {
+  const bgImage = {
+    backgroundImage: `url(${Vector})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%",
+  };
+  return (
+    <>
+      <div className="min-h-[550px]">
+        <div className="min-h-[550px] flex justify-center items-center backdrop-blur-xl py-12 sm:py-0 ">
+          <div
+            data-aos="slide-up"
+            data-aos-duration="300"
+            className="container"
+          >
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              {/* Image section */}
+              <div>
+                <img
+                  src={BiryaniImg}
+                  alt="biryani img"
+                  className="max-w-[430px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)]"
+                />
+              </div>
+              {/* text content section */}
+              <div className="flex flex-col justify-center gap-6 sm:pt-0">
+                <h1 className="text-3xl sm:text-4xl font-bold">
+                  
+                  "Culinary Delights at Your Doorstep!"
+                </h1>
+                <p className="text-sm text-gray-500 tracking-wide leading-5">
+                Explore our diverse menu for a delightful experience. Order now 
+                and savor the flavors of our expertly crafted dishes in the comfort of your home.
+                  <br />
+                  <br />
+                  Embark on a gastronomic journey with our restaurant's diverse menu, 
+                  offering delectable dishes crafted with passion. From savory delights 
+                  to sweet temptations, our culinary creations promise an exquisite dining experience. 
+                  Order now and treat your taste buds to a symphony of flavors delivered to your doorstep.
+                </p>
+                <div className="flex gap-6">
+                  <div>
+                    <GrSecure className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-violet-100 dark:bg-violet-400" />
+                  </div>
+                  <div>
+                    <IoFastFood className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-orange-100 dark:bg-orange-400" />
+                  </div>
+                  <div>
+                    <GiFoodTruck className="text-4xl h-20 w-20 shadow-sm p-5 rounded-full bg-green-100 dark:bg-green-400" />
+                  </div>
+                </div>
+                <div>
+                  <button className="bg-gradient-to-r from-primary to-secondary text-white py-2 px-4 rounded-full shadow-xl hover:shadow-md">
+                    Order Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Img from "../../assets/biryani.png";
+import Img2 from "../../assets/biryani2.png";
+import Img3 from "../../assets/biryani4.png";
+import StarRatings from "react-star-ratings";
+const ServicesData = [
+  {
+    id: 1,
+    img: Img2,
+    name: "Biryani",
+    description:
+      "Savor aromatic perfection with our biryani – a symphony of flavorful spices, tender meat, and fragrant rice, creating unparalleled satisfaction.",
+  },
+  {
+    id: 2,
+    img: Img2,
+    name: "Chiken kari",
+    description:
+      "Delight in the rich flavors of our Chicken Kari. Succulent pieces simmered in a fragrant curry, creating an unforgettable dining experience.",
+  },
+  {
+    id: 3,
+    img: Img2,
+    name: "Cold Cofee",
+    description:
+      "Indulge in the refreshing allure of our Cold Coffee. Smooth, chilled perfection blended to satisfy your caffeine cravings with delightful decadence.",
+  },
+];
+const Services = () => {
+  return (
+    <>
+      <span id="services"></span>
+      <div className="py-10">
+        <div className="container">
+          <div className="text-center mb-20 max-w-[400px] mx-auto">
+            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary ">
+              Our Services
+            </p>
+            <h1 className="text-3xl font-bold">Services</h1>
+            <p className="text-xs text-gray-400">
+              Indulge in our restaurant's exceptional services – 
+              from prompt and friendly staff to seamless reservations – ensuring a delightful dining experience.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 md:gap-5 place-items-center">
+            {ServicesData.map((service) => (
+              <div
+                data-aos="zoom-in"
+                data-aos-duration="300"
+                className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-primary dark:hover:bg-primary hover:text-white relative shadow-xl duration-high group max-w-[300px]"
+              >
+                <div className="h-[100px]">
+                  <img
+                    src={service.img}
+                    alt=""
+                    className="max-w-[200px] block mx-auto transform -translate-y-14
+                  group-hover:scale-105 group-hover:rotate-6 duration-300"
+                  />
+                </div>
+                <div className="p-4 text-center">
+                  <div className="w-full ">
+                    {/* <StarRatings
+                      rating={4}
+                      starRatedColor="yellow"
+                      isSelectable={false}
+                      starHoverColor="yellow"
+                      // starSelectingHoverColor
+                      starDimension="20px"
+                      changeRating={() => {}}
+                      numberOfStars={5}
+                      name="rating"
+                    /> */}
+                  </div>
+                  <h1 className="text-xl font-bold">{service.name}</h1>
+                  <p className="text-gray-500 group-hover:text-white duration-high text-sm line-clamp-2">
+                    {service.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
